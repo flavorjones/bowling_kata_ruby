10f074baf0bdb1a30deec5be8893fb3abf0cc5f1
Davis W. Frank <dwfrank@pivotal.io>
2016-10-09 09:53:33 -0700

    rspec working in Rubymine

--- spec/rspec/bowling_spec.rb
+++ spec/rspec/bowling_spec.rb
@@ -3,7 +3,12 @@ require 'rspec'
 describe 'My behaviour' do
 
   it 'should do something' do
-
     expect(3).to eq(3)
   end
+
+  it 'should fail something' do
+    expect(3).to eq(5)
+  end
+
+
 end
